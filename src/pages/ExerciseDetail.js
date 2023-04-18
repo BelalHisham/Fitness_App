c20068dacbfcb25dbad8f5e903772f3a941786d0
Belal Hisham <bh2062001.bh@gmail.com>
2023-04-18 17:07:14 +0200

    Adding exercise videos from youtube to our exercises, using Youtube Search and Download from Rapid API

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -2,7 +2,7 @@ import React, {useEffect, useState} from 'react'
 import { useParams } from 'react-router-dom';
 import { Box } from '@mui/material';
 
-import {exerciseOptions, fetchData} from '../utils/fetchData';
+import {exerciseOptions, fetchData, youtubeOptions} from '../utils/fetchData';
 import Detail from '../components/Detail';
 import ExerciseVideos from '../components/ExerciseVideos';
 import SimilarExercises from '../components/SimilarExercises';
@@ -13,7 +13,9 @@ const ExerciseDetail = () => {
 
   
 const [exerciseDetail, setExerciseDetail] = useState({ });
+const [exerciseVideos, setexerciseVideos] = useState([ ])
 const { id } = useParams();
+
 
 // We will recall this function whever the id changes
 useEffect(() => {
@@ -22,17 +24,23 @@ useEffect(() => {
     const exerciseDbUrl = 'https://exercisedb.p.rapidapi.com';
     const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapidapi.com';
 
-    const exerciseDetialData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
-    setExerciseDetail(exerciseDetialData)
+    const exerciseDetailData = await fetchData(`${exerciseDbUrl}/exercises/exercise/${id}`, exerciseOptions);
+    setExerciseDetail(exerciseDetailData);
+
+    const exerciseVideosData = await fetchData(`${youtubeSearchUrl}/search?query=${exerciseDetailData.name} exercise`, youtubeOptions);
+    setexerciseVideos (exerciseVideosData.contents);
+
 
   }
   fetchExercisesData();
 }, [id])
 
+
+
   return (
     <Box>
       <Detail exerciseDetail={exerciseDetail} />
-      <ExerciseVideos />
+      <ExerciseVideos exerciseVideos = {exerciseVideos} name = {exerciseDetail.name} />
       <SimilarExercises />
     </Box>
   )
