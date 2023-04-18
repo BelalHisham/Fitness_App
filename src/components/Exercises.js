4d7545ef6dbf49c8fe9922a39455ef376544e62d
Belal Hisham <bh2062001.bh@gmail.com>
2023-04-18 15:54:44 +0200

    adding exercise details to each exercise when clicked.

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -12,21 +12,22 @@ const Exercises = ({exercises, setExercises, bodyPart}) => {
   const [currentPage, setCurrentPage] = useState(1);
   const exercisesPerPage = 9;
 
-  useEffect(() => {
-    const fetchExercisesData = async () => {
-      let exercisesData = [];
+  // useEffect(() => {
+  //   const fetchExercisesData = async () => {
+  //     let exercisesData = [];
 
-      if (bodyPart === 'all') {
-        exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
-      } else {
-        exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, exerciseOptions);
-      }
+  //     if (bodyPart === 'all') {
+  //       exercisesData = await fetchData('https://exercisedb.p.rapidapi.com/exercises', exerciseOptions);
+  //     } else {
+  //       exercisesData = await fetchData(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, exerciseOptions);
+  //     }
 
-      setExercises(exercisesData);
-    };
+  //     setExercises(exercisesData);
+      
+  //   };
 
-    fetchExercisesData();
-  }, [bodyPart]);
+  //   fetchExercisesData();
+  // }, [bodyPart]);
 
 
 
