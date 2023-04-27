77bfcc137a2c928dfa98e82721ba0ebe7b53d217
Belal Hisham <bh2062001.bh@gmail.com>
2023-04-27 16:28:10 +0200

    add context for auth

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from "react-dom/client"
 import App from "./App"
 import { BrowserRouter } from "react-router-dom";
 import { WorkoutContextProvider } from "./context/WorkoutContext";
+import { AuthContextProvider } from "./context/AuthContext";  
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <BrowserRouter>
-    <WorkoutContextProvider>
-    <App />
-    </WorkoutContextProvider>
-    </BrowserRouter>
+    
+    <AuthContextProvider>
+        <BrowserRouter>
+        <WorkoutContextProvider>
+        <App />
+        </WorkoutContextProvider>
+        </BrowserRouter>
+    </AuthContextProvider>
+    
 );  
