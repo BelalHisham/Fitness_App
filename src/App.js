4660768abb2a76108383590f29af7011d3ecbb25
Belal Hisham <bh2062001.bh@gmail.com>
2023-04-28 02:29:14 +0200

    Navigate to login if not logged in, and navigate to add_workout if logged in

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import { Box } from '@mui/material';
 import Navbar from "./components/Navbar";
 import ExerciseDetail from './pages/ExerciseDetail';
@@ -9,9 +9,11 @@ import Footer from './components/Footer';
 import "./App.css";
 import Login from './pages/Login';
 import Signup from './pages/Signup'
+import { useAuthContext } from './hooks/useAuthContext';
 
 
 const App = () => {
+  const {user} = useAuthContext()
   return (
     <Box width = "400px" sx={{width: {xl: '1488px'}}} m = "auto">
         <Navbar />
@@ -19,9 +21,9 @@ const App = () => {
         <Routes>
             <Route path= "/" element = {<Home/>} />
             <Route path= "/exercise/:id" element = {<ExerciseDetail/>} />
-            <Route path= "/add_workout" element = {<AddWorkout />} />
-            <Route path= "/login" element = {<Login />} />
-            <Route path= "/signup" element = {<Signup />} />
+            <Route path= "/add_workout" element = {user ? <AddWorkout /> : <Navigate to="/login" /> } />
+            <Route path= "/login" element = {!user ? <Login /> : <Navigate to = "/add_workout" />} />
+            <Route path= "/signup" element = {!user ? <Signup /> : <Navigate to = "/add_workout" />} />
        
         </Routes>
         
